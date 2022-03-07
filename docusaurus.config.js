dc4812c25b22cb0bb3cf1ded2ddfb53de6e7f82f
fuzhenn <fuzhen@maptalks.org>
2022-03-07 15:24:07 +0800

    修改备案号

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ module.exports = {
       ],
     },
     footer: {
-      copyright: `2022&ensp;&copy;&ensp;maptalks.com版权所有&ensp;&ensp;&ensp;&ensp;鄂ICP备14002653号-6&ensp;&ensp;&ensp;&ensp;鄂公网安备&ensp;35020302033941号`, // You can also put own HTML here
+      copyright: `2022&ensp;&copy;&ensp;maptalks.com&ensp;版权所有&ensp;&ensp;&ensp;&ensp;鄂ICP备2020021619号`, // You can also put own HTML here
     },
   },
   i18n: {
